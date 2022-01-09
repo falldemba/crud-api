552271066da41c27ef20001eacb9d0158654b3d4
kaysermax <demba@next-version.com>
2022-01-09 04:42:21 +0000

    fix: deploy errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = require('./app/routes/router')
 const mongooseConfig = require('./app/config/db.config')
 const cors = require('cors')
-const PORT = 7000
+const PORT = process.env.PORT || 8000
 
 const app = express()
 
